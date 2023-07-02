63bf30dd176d1f39846e5912fe046d118366c38d
Naveen Chandra <the.naveenchandra@gmail.com>
2023-07-02 19:12:23 +0530

    Setting up the env.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -10,6 +11,12 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>
+          Twitter Clone
+        </title>
+        <meta name="description" content="This is the twitter clone by Nav Chandra" />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   );
