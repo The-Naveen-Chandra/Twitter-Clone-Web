0b014de56fe1d4f919077232c39a0c36a125a00a
Naveen Chandra <the.naveenchandra@gmail.com>
2023-07-22 17:06:27 +0530

    Dev. Following Section and its calling function

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,6 +1,9 @@
+import { type Prisma } from "@prisma/client";
+import { type inferAsyncReturnType } from "@trpc/server";
 import { z } from "zod";
 
 import {
+  type createTRPCContext,
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
@@ -10,57 +13,29 @@ export const tweetRouter = createTRPCRouter({
   infinitFeed: publicProcedure
     .input(
       z.object({
+        onlyFollowing: z.boolean().optional(),
         limit: z.number().optional(),
         cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
       })
     )
-    .query(async ({ input: { limit = 10, cursor }, ctx }) => {
-      const currentUserId = ctx.session?.user.id;
-
-      const data = await ctx.prisma.tweet.findMany({
-        take: limit + 1,
-        cursor: cursor ? { createdAt_id: cursor } : undefined,
-        orderBy: [{ createdAt: "desc" }, { id: "desc" }],
-        select: {
-          id: true,
-          content: true,
-          createdAt: true,
-          _count: { select: { likes: true } },
-          likes:
-            currentUserId == null
-              ? false
-              : { where: { userId: currentUserId } },
-          user: {
-            select: { name: true, id: true, image: true },
-          },
-        },
-      });
-
-      let nextCursor: typeof cursor | undefined;
-      if (data.length > limit) {
-        const nextItem = data.pop();
-        if (nextItem != null) {
-          nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
-        }
+    .query(
+      async ({ input: { limit = 10, onlyFollowing = false, cursor }, ctx }) => {
+        const currentUserId = ctx.session?.user.id;
+        return await getInfiniteTweets({
+          limit,
+          ctx,
+          cursor,
+          whereClause:
+            currentUserId == null || !onlyFollowing
+              ? undefined
+              : {
+                  user: {
+                    followers: { some: { id: currentUserId } },
+                  },
+                },
+        });
       }
-
-      return {
-        tweets: data.map((tweet) => {
-          return {
-            id: tweet.id,
-            content: tweet.content,
-            createdAt: tweet.createdAt,
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            likeCount: tweet._count.likes,
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            user: tweet.user,
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-            likedByMe: tweet.likes?.length > 0,
-          };
-        }),
-        nextCursor,
-      };
-    }),
+    ),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(async ({ input: { content }, ctx }) => {
@@ -89,3 +64,65 @@ export const tweetRouter = createTRPCRouter({
       }
     }),
 });
+
+async function getInfiniteTweets({
+  whereClause,
+  ctx,
+  limit,
+  cursor,
+}: {
+  whereClause?: Prisma.TweetWhereInput;
+  limit: number;
+  cursor:
+    | {
+        id: string;
+        createdAt: Date;
+      }
+    | undefined;
+  ctx: inferAsyncReturnType<typeof createTRPCContext>;
+}) {
+  const currentUserId = ctx.session?.user.id;
+
+  const data = await ctx.prisma.tweet.findMany({
+    take: limit + 1,
+    cursor: cursor ? { createdAt_id: cursor } : undefined,
+    orderBy: [{ createdAt: "desc" }, { id: "desc" }],
+    where: whereClause,
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      _count: { select: { likes: true } },
+      likes:
+        currentUserId == null ? false : { where: { userId: currentUserId } },
+      user: {
+        select: { name: true, id: true, image: true },
+      },
+    },
+  });
+
+  let nextCursor: typeof cursor | undefined;
+  if (data.length > limit) {
+    const nextItem = data.pop();
+    if (nextItem != null) {
+      nextCursor = { id: nextItem.id, createdAt: nextItem.createdAt };
+    }
+  }
+
+  return {
+    tweets: data.map((tweet) => {
+      return {
+        id: tweet.id,
+        content: tweet.content,
+        createdAt: tweet.createdAt,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        likeCount: tweet._count.likes,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        user: tweet.user,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+        likedByMe: tweet.likes?.length > 0,
+      };
+    }),
+    nextCursor,
+  };
+}
