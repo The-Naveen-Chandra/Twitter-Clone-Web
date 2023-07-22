3cb96ebeb9eeebd814fff1946b86d59899de4e78
Naveen Chandra <the.naveenchandra@gmail.com>
2023-07-22 16:31:41 +0530

    Dev. Styling components for home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { type NextPage } from "next";
+import { InfiniteTweetList } from "~/components/InfiniteTweetList";
 import { NewTweetForm } from "~/components/NewTweetForm";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   return (
@@ -8,8 +10,28 @@ const Home: NextPage = () => {
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewTweetForm />
+      <RecentTweet />
     </>
   );
 };
+
+function RecentTweet() {
+  const tweets = api.tweet.infinitFeed.useInfiniteQuery(
+    {},
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+
+  return (
+    <InfiniteTweetList
+      tweets={tweets.data?.pages.flatMap((page) => page.tweets)}
+      isError={tweets.isError}
+      isLoading={tweets.isLoading}
+      hasMore={tweets.hasNextPage}
+      fetchNewTweet={tweets.fetchNextPage}
+    />
+  );
+}
 
 export default Home;
